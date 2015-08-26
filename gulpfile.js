c03913b3caac4dced11b47b2647f04cbd5073e13
Michael K. Clements <michaelkc@me.com>
2015-08-27 00:10:24 +0100

    Updated so it t uses gulp and Javascript was changed to work

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp.src("sass/*.scss")
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest("css"))
+    .pipe(browserSync.stream());
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("sass/*.scss", ['sass']);
+    gulp.watch("js/*.js", browserSync.reload);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
